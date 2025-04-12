807e1352d858731ce0f610dc318c773ba1cd977b
Pngtaq <110612377+Pngtaq@users.noreply.github.com>
2025-04-12 22:37:33 +0800

    auth controller and user model

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
+connectDB();
 
-app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ credentials: true }));
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Hello from Express!" });
 });
 
